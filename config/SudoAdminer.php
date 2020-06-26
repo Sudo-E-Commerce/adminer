60e319c7ecf7e563aa92a4b6a613e9aeb54e29bc
Nguyễn Viết Hiếu <36782331+hieunvph04479@users.noreply.github.com>
2020-06-26 11:41:15 +0700

    cập nhật đường dẫn admin bắt theo admin_dir

--- config/SudoAdminer.php
+++ config/SudoAdminer.php
@@ -6,7 +6,8 @@ return [
      | -----------------------------------------------------------------
      */
     'route' => [
-    	'prefix' 		=> '/admin/adminer',
+    	'prefix' 		=> env('SUDO_ADMINER_PATH') ? env('SUDO_ADMINER_PATH') : '/'.env('ADMIN_DIR', 'admin').'/adminer',
+    	
 		'middleware' 	=> env('SUDO_ADMINER_MIDDLEWARE') ? explode(',', env('SUDO_ADMINER_MIDDLEWARE')) : ['web', 'auth-admin', 'only-dev'],
     ],
 ];
